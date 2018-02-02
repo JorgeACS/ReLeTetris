b5d03070fb261b57e60a3a77a0eed605a747c33b
JorgeACS <jorge_nintendo@hotmail.com>
2018-02-02 08:08:57 -0700

    Agrega dibujado de tetrominos WIP

--- test.go
+++ test.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"github.com/faiface/pixel/text"
 	"golang.org/x/image/colornames"
+	"golang.org/x/image/font/basicfont"
 	"image"
 	_ "image/png"
 	"math/rand"
@@ -21,6 +24,9 @@ const MIN_GRAVITY = 0.125
 const MAX_GRAVITY = 20
 const NUM_PIECES = 7
 const NEXT_LENGTH = 5
+
+var s1 = rand.NewSource(time.Now().UnixNano())
+var r1 = rand.New(s1)
 
 var IStates [4][16]int = [4][16]int{
 	{
@@ -248,7 +254,8 @@ func updateGame(game Game, milliseconds int64) {
 //Structure for the playfield
 //Contains the stack, which is composed of one or more Tetrominos
 type Playfield struct {
-	stack []Tetromino
+	stack        []Tetromino
+	currentPiece Tetromino
 }
 
 //--------------Tetromino---------------
@@ -259,10 +266,12 @@ type Tetromino struct {
 	state  int
 	x      int
 	y      int
+	mat    pixel.Matrix
 }
 
 func pushPieceDown(piece Tetromino) {
 	piece.y -= 1
+	piece.mat = piece.mat.Moved(pixel.V(0, -BLOCK_SIZE))
 }
 func run() {
 	cfg := pixelgl.WindowConfig{
@@ -282,9 +291,23 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
+
+	basicAtlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
+	basicTxt := text.New(pixel.V(100, 500), basicAtlas)
 	game := createGame()
 	last := time.Now()
+	fmt.Fprintln(basicTxt, "current piece: "+game.currentPiece)
+	fmt.Fprintln(basicTxt, "first word bag: ")
+	for i := 0; i < NUM_PIECES; i++ {
+		fmt.Fprintf(basicTxt, "%s, ", game.currentBag[i])
+	}
+	fmt.Fprintln(basicTxt, "\nsecond word bag: ")
+	for i := 0; i < NUM_PIECES; i++ {
+		fmt.Fprintf(basicTxt, "%s, ", game.nextBag[i])
+	}
+
 	for !win.Closed() {
+		win.Clear(colornames.Skyblue)
 		//Draw
 		mat := pixel.IM
 		mat = mat.Moved(win.Bounds().Center())
@@ -297,23 +320,32 @@ func run() {
 			}
 			mat = mat.Moved(pixel.V(-BLOCK_SIZE*PLAYFIELD_WIDTH, -BLOCK_SIZE))
 		}
+		drawStack(game)
+
 		//Update
+		tetromino := createTetromino("O")
+		drawTetromino(win, MINO_SPRITE, tetromino)
+		basicTxt.Draw(win, pixel.IM)
 		updateGame(game, time.Since(last).Nanoseconds()*1000000)
 		win.Update()
 	}
 
 }
 
+func drawStack(game Game) {
+
+}
 func generate7PieceBag() []string {
 	bag := []string{"I", "O", "T", "L", "J", "S", "Z"}
-	shuffleBag(bag, 100)
+	shuffleBag(bag, 10)
 	return bag
 
 }
 func shuffleBag(bag []string, shuffles int) {
+
 	for aux := 0; aux < shuffles; aux++ {
-		i := rand.Intn(NUM_PIECES)
-		j := rand.Intn(NUM_PIECES)
+		i := r1.Intn(NUM_PIECES)
+		j := r1.Intn(NUM_PIECES)
 		bag[i], bag[j] = bag[j], bag[i]
 	}
 
@@ -321,12 +353,12 @@ func shuffleBag(bag []string, shuffles int) {
 func createPlayfield() Playfield {
 	return Playfield{stack: []Tetromino{}}
 }
-func pushTetromino(playfield Playfield, piece Tetromino) {
+func dropTetromino(playfield Playfield, piece Tetromino) {
 	playfield.stack = append(playfield.stack, piece)
 }
 
 //positions the tettromino grid on the bottom left corner
-func positionBottomLeft(win pixelgl.Window, mat pixel.Matrix) pixel.Matrix {
+func positionBottomLeft(win *pixelgl.Window, mat pixel.Matrix) pixel.Matrix {
 	mat = mat.Moved(win.Bounds().Center())
 	mat = mat.Moved(pixel.V(-400, 600))
 	for i := 0; i < PLAYFIELD_HEIGHT-GRID_SIZE; i++ {
@@ -335,29 +367,57 @@ func positionBottomLeft(win pixelgl.Window, mat pixel.Matrix) pixel.Matrix {
 	return mat
 }
 
-func moveTetrominoGrid(mat pixel.Matrix, xMagnitude int, yMagnitude int) pixel.Matrix {
-	vector := pixel.V(float64(BLOCK_SIZE*xMagnitude), float64(BLOCK_SIZE*yMagnitude*1.0))
-	mat = mat.Moved(vector)
-	return mat
-}
-
-/*
-func drawTetromino(win, piece Tetromino, state int, x int, y int) {
-	mat := pixel.IM
-	mat = positionBottomLeft(win, mat)
-	mat = moveTetrominoGrid(mat, piece.x, piece.y)
-
+func drawTetromino(win *pixelgl.Window, sprite *pixel.Sprite, piece Tetromino) {
+	var gridToBeDrawn [16]int
+	pieceColor := colornames.Darkgreen
 	for i := 0; i < GRID_SIZE; i++ {
 		for j := 0; j < GRID_SIZE; j++ {
-			if Tetromino.piece == 'S' {
-
+			switch piece.letter {
+			case "S":
+				gridToBeDrawn = SStates[piece.state]
+				pieceColor = colornames.Darkgreen
+			case "Z":
+				gridToBeDrawn = ZStates[piece.state]
+				pieceColor = colornames.Darkred
+			case "L":
+				gridToBeDrawn = LStates[piece.state]
+				pieceColor = colornames.Darkblue
+			case "J":
+				gridToBeDrawn = JStates[piece.state]
+				pieceColor = colornames.Orange
+			case "O":
+				gridToBeDrawn = OStates[piece.state]
+				pieceColor = colornames.Lightyellow
+			case "T":
+				gridToBeDrawn = TStates[piece.state]
+				pieceColor = colornames.Purple
+			case "I":
+				gridToBeDrawn = IStates[piece.state]
+				pieceColor = colornames.Lightblue
+			default:
+				gridToBeDrawn = TStates[piece.state]
+				pieceColor = colornames.Darkred
 			}
 		}
 	}
+	block_mat := piece.mat
+	block_mat = block_mat.ScaledXY(win.Bounds().Center(), pixel.V(0.25, 0.25))
+	for i := 0; i < GRID_SIZE; i++ {
+		for j := 0; j < GRID_SIZE; j++ {
+			if gridToBeDrawn[i*GRID_SIZE+j] != 0 {
+				sprite.DrawColorMask(win, piece.mat, pieceColor)
+				block_mat = block_mat.Moved(pixel.V(BLOCK_SIZE, 0))
+			}
+		}
+		block_mat = block_mat.Moved(pixel.V(-BLOCK_SIZE*GRID_SIZE, -BLOCK_SIZE))
+	}
 
-}*/
+}
 func createTetromino(letter string) Tetromino {
-	return Tetromino{letter, 0, PLAYFIELD_WIDTH / 2, PLAYFIELD_HEIGHT}
+	mat := pixel.IM
+	mat = mat.Moved(win.Bounds().Center())
+	mat = mat.Moved(pixel.V(-400, 600))
+	return Tetromino{letter, 0, PLAYFIELD_WIDTH / 2, PLAYFIELD_HEIGHT, mat}
 }
 func main() {
 	pixelgl.Run(run)
